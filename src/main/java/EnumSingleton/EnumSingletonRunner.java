ed6ecd70a687bc361a37419e25ac07bc5ee22bf1
DimaShevko <dmishevko@gmail.com>
2021-08-21 16:33:35 +0300

    fix critical bug

--- src/main/java/EnumSingleton/EnumSingletonRunner.java
+++ src/main/java/EnumSingleton/EnumSingletonRunner.java
@@ -3,7 +3,6 @@ package EnumSingleton;
 public class EnumSingletonRunner {
     public static void main(String[] args) {
         SingletonEnum singleton = SingletonEnum.INSTANCE;
-
         System.out.println(singleton.getValue());
         singleton.setValue(2);
         System.out.println(singleton.getValue());
